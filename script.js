fafb81c1b370e8ca009c5c1e1add020f17c7e40d
sdabrowski <sdabrow2@gmail.com>
2017-05-12 10:39:59 -0500

    Question 2 Answered

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-//function factorial(n) {
-//    var value = n;
-//    
-//    for(i=1; i<n; i++){
-//        value = value * (n-i);
-//    }
-//    
-//    alert("The factorial value is: " + value);
-//}
-
 //Question 1 - Recursive
 function factorial(n){
     if(n === 1)
@@ -16,12 +6,17 @@ function factorial(n){
 }
 
 //Question 2
-//function range(x, y) {
-//    if (x+1 === y)  return;
-//    console.log(y-1);
-//    range(x, y-1);
-//}
+function range(a, b) {
+    var result = "";
+    if (a == b)
+        return a;
+    else if (a > b)
+        return result = a + ", " + range (a-1,b);
+    else if (a < b)
+        return result = b  + ", " + range (a, b-1);
+}
 
+//Question 3
 function exponentProgram(x, y){
     if(y === 1)
         return x;
